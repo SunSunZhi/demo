04e93539f3dd5325a61bf739c238bb627000677d
SunSunZhi <1812023427@qq.com>
2019-11-22 16:00:15 +0800

    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Branch2!");
+        System.out.println("Hello World2");
 		System.out.println("1");
-		System.out.println("Branch2!");
+		System.out.println("Hello World2");
 		System.out.println("2");
     }
 }
